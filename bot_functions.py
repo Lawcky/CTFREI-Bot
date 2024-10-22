a9f252f19607879ed9622d16cae117ed4aff731b
Lawcky <147432762+Lawcky@users.noreply.github.com>
2024-10-22 23:38:12 +0200

    Update bot_functions.py

--- bot_functions.py
+++ bot_functions.py
@@ -86,27 +86,42 @@ async def search_ctf_data(filename, query, WEIGHT_RANGE):
                 for event in events:
                     event_weight = float(event['weight'])  # Convert event weight to float
                     if min_weight <= event_weight <= max_weight:
+                        print(event)
                         match.append({
-                            'name': event['name'],
-                            'date': event['date'],
-                            'format': event['format'],
-                            'location': event['location'],
-                            'weight': event['weight'],
-                            'link': event['link']
+                            # event info
+                            "title": event['title'],
+                            "weight": event['weight'],
+                            "url": event['url'],
+                            "ctftime_url": event['ctftime_url'],
+                            "start": event['start'],
+                            "finish": event['finish'],
+                            "duration": event['duration'],
+                            "format": event['format'],
+                            "location": event['location'],
+                            "logo": event['logo'],
+                            "description": event['description'],
+                            "onsite": event['onsite'],
                         })
             except ValueError:
                 print(f"no result found: {query}") 
 
         else:
             for event in events:
-                if (query.lower() in event['name'].lower()):
+                if (query.lower() in event['title'].lower()):
                     match.append({
-                        'name': event['name'],
-                        'date': event['date'],
-                        'format': event['format'],
-                        'location': event['location'],
-                        'weight': event['weight'],
-                        'link': event['link']
+                        # event info
+                        "title": event['title'],
+                        "weight": event['weight'],
+                        "url": event['url'],
+                        "ctftime_url": event['ctftime_url'],
+                        "start": event['start'],
+                        "finish": event['finish'],
+                        "duration": event['duration'],
+                        "format": event['format'],
+                        "location": event['location'],
+                        "logo": event['logo'],
+                        "description": event['description'],
+                        "onsite": event['onsite'],
                     })
 
         return match
@@ -166,80 +181,108 @@ def get_channel_by_name(guild: discord.Guild, channelname: str):
     return None
 
 # retrieve the data of an event using the discord role associated to it (not case sensitive)
-async def search_event_data(filename, role: discord.Role):
+async def search_event_data(filename: str, role: str):
     with open(filename, 'r') as events_file:
         events_data = json.load(events_file)
 
     for event in events_data:
         for rolename in event:
-            if rolename.lower() == role.name.lower():
-                return event[role.name] #returns the information about the CTF
+            if rolename.lower() == role.lower():
+                return event[role] #returns the information about the CTF
 
     return None
 
 # creates & returns an embedded message based on the event_info given (in the format of ctf_events.json), the integer is to differenciate the color. 
 async def send_event_info(event_info, id: int):
 
-    # Split the start and end times using the "—" symbol & set the date format
-    print(event_info['date'])
-    time_range = event_info['date'].split(" — ")
-    start_date_format = "%d %b., %H:%M %Z"
-    end_date_format = "%d %b. %Y, %H:%M %Z"  # When the year is included in the end date
+    # Retrieve start, finish, and duration from event_info
+    start_time = datetime.fromisoformat(event_info['start'])  # ISO format to datetime
+    end_time = datetime.fromisoformat(event_info['finish'])   # ISO format to datetime
+    duration_days = event_info['duration'].get('days', 0)
+    duration_hours = event_info['duration'].get('hours', 0)
 
-    try:
-        end_time = datetime.strptime(time_range[1], end_date_format)
-        
-        start_time = datetime.strptime(time_range[0], start_date_format)
-        start_time = start_time.replace(year=end_time.year)
+    # Combine duration to a single string
+    duration_str = f"{duration_days * 24 + duration_hours} hours"
 
-        print(start_time, end_time)
+    # Format the start and end time as 'hrs:minutes days-month-year'
+    formatted_start_time = start_time.strftime('%d-%m-%Y %H:%M')
+    formatted_end_time = end_time.strftime('%d-%m-%Y %H:%M')
+    start = start_time.timestamp()
+    end = end_time.timestamp()
+    current = datetime.now().timestamp()
+    
+    # formatted_current_time = datetime.now().strftime('%d-%m-%Y %H:%M')
+    # Print times for reference
+    # print(f"Current Time: {formatted_current_time}")
+    # print(f"Start Time: {formatted_start_time}")
+    # print(f"End Time: {formatted_end_time}")
 
-        duration = end_time - start_time
-        duration_hours = duration.total_seconds() // 3600
-        duration_str = f"{int(duration_hours)} hours"
+    # Calculate event status based on the current time
+    if current < start:
+        # Event hasn't started yet, calculate time until start
+        time_until_start = start - current
+        hours_until_start = time_until_start// 3600
+        minutes_until_start = (time_until_start % 3600) // 60
+        status_str = f"Starts in {int(hours_until_start)} hours and {int(minutes_until_start)} minutes"
 
-        # Get the current time
-        current_time = datetime.now()
+    elif start <= current < end:
+        # Event has started, calculate time left until it ends
+        time_left = end - current
+        hours_left = time_left // 3600
+        minutes_left = (time_left % 3600) // 60
+        status_str = f"{int(hours_left)}hrs & {int(minutes_left)}min left"
 
-        # Calculate event status based on current time
-        if current_time < start_time:
-            # Event hasn't started yet, calculate time until start
-            time_until_start = start_time - current_time
-            hours_until_start = time_until_start.total_seconds() // 3600
-            minutes_until_start = (time_until_start.total_seconds() % 3600) // 60
-            status_str = f"Starts in {int(hours_until_start)} hours and {int(minutes_until_start)} minutes"
-        
-        elif start_time <= current_time < end_time:
-            # Event has started, calculate time left until it ends
-            time_left = end_time - current_time
-            hours_left = time_left.total_seconds() // 3600
-            minutes_left = (time_left.total_seconds() % 3600) // 60
-            status_str = f"{int(hours_left)}hrs & {int(minutes_left)}min left"
-        
-        else:
-            # Event is over
-            status_str = "Event is over"
-
-    except (ValueError, IndexError):
-        duration_str = "N/A" 
+    else:
+        # Event is over
+        status_str = "Event is over"
 
     # Set up the embedded message
     color = discord.Color.dark_gold() if not id else discord.Color.blurple() # if id 0 then dark_gold, else blurple()
     embeded_message = discord.Embed(
-        title=f"__{event_info['name']}__", 
-        url=event_info['link'],
-        description=f"Here are the information on {event_info['name']}.",
+        title=f"__{event_info['title']}__", 
+        url=event_info['url'],
+        description=f"Here are the information on {event_info['title']}.",
         color=color
     )
     
-    embeded_message.set_author(name="CTF INFORMATION", url=event_info['link'])
+    embeded_message.set_author(name="CTF INFORMATION", url=event_info['ctftime_url'])
     embeded_message.add_field(name="Weight", value=f"**{event_info['weight']}**", inline=True)
-    embeded_message.add_field(name="Location", value=f"**{event_info['location']}**", inline=True)
+    embeded_message.add_field(name="Onsite", value=f"**{event_info['onsite']}**", inline=True)
     embeded_message.add_field(name="Format", value=f"**{event_info['format']}**", inline=True)
-    embeded_message.add_field(name="Start time", value=f"**{start_time}**", inline=True)
-    embeded_message.add_field(name="End time", value=f"**{end_time}**", inline=True)
+    embeded_message.add_field(name="Start time", value=f"**{formatted_start_time}**", inline=True)
+    embeded_message.add_field(name="End time", value=f"**{formatted_end_time}**", inline=True)
     embeded_message.add_field(name="Duration", value=f"**{duration_str}**", inline=True)
     embeded_message.add_field(name="Status", value=f"**{status_str}**", inline=False)
     embeded_message.set_image(url="https://cdn.discordapp.com/attachments/1167256768087343256/1202189774836731934/CTFREI_Banniere_920_x_240_px_1.png?ex=67162479&is=6714d2f9&hm=c649d21b2152c0200b9466a29c09a04865387410258c1c228c8df58db111c539&")
 
     return embeded_message
+
+def api_call(url, file):
+    try:
+        url = "https://ctftime.org/api/v1/events/?limit=100"
+
+        headers = {
+            'User-Agent': 'curl/7.68.0'  # Mimicking a curl request
+        }
+
+        response = requests.get(url, headers=headers)
+
+        # Check request's status code
+        if response.status_code != 200:
+            print(f"Failed to retrieve content: {response.status_code}")
+            return None
+            
+        data = response.json()
+
+        #print(data)
+        with open(file, 'w') as fp:
+             json.dump(data, fp, indent=4)
+
+        print(f"{url} data has been saved to {file}")
+        
+        return data
+
+
+    except Exception as e:
+            print(f"An error occurred: {e}")
+            return None
