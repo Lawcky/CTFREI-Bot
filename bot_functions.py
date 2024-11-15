7ac77840dfff6c6f7ab1e2317d5d7ca329ebcbd9
Lawcky <147432762+Lawcky@users.noreply.github.com>
2024-11-15 12:13:56 +0100

    Update bot_functions.py

--- bot_functions.py
+++ bot_functions.py
@@ -1,5 +1,4 @@
 import requests
-from bs4 import BeautifulSoup
 import discord
 import json
 from datetime import datetime
@@ -11,7 +10,7 @@ def generate_unique_id(CTFname: str):
     unique_id = (hashlib.md5(CTFname.encode()).hexdigest())[:8]
     return unique_id
 
-def list_directory_contents(directory):
+def list_directory_contents(directory: str):
     try:
         contents = list(scandir(directory))
         return [item.name for item in contents]
@@ -22,76 +21,8 @@ def list_directory_contents(directory):
         print(f"You don't have permission to access the directory '{directory}'.")
         return []
 
-# retrive the upcoming CTF events and data, this is a scraping function, will be later changed for API calls
-async def extract_ctf_data(url, filename):
-    try:
-        headers = {
-            'User-Agent': 'curl/7.68.0'  # Mimicking a curl request
-        }
-
-        response = requests.get(url, headers=headers)
-
-        # Check request's status code
-        if response.status_code != 200:
-            print(f"Failed to retrieve content: {response.status_code}")
-            return None
-
-        soup = BeautifulSoup(response.content, 'html.parser')
-
-        table = soup.find('table', {'class': 'table table-striped'})
-        if table is None:
-            print("Table not found on the page")
-            return None
-
-        ctf_data = []
-        rows = table.find_all('tr')[1:]  # Skip the first header row
-
-        if not rows:
-            print("No rows found in the table.")
-            return None
-
-        for row in rows:
-            columns = row.find_all('td') # retrieve all data
-            links = row.find('a') # retrieve the links to get an easy access
-            # Extract data for each field
-            event_name = columns[0].get_text(strip=True)
-            event_date = columns[1].get_text(strip=True)
-            event_format = columns[2].get_text(strip=True)
-            event_location = columns[3].get_text(strip=True)
-            event_weight = columns[4].get_text(strip=True)
-            event_notes = columns[5].get_text(strip=True)
-            event_link = links.get('href')
-
-            # Add the extracted data to the list
-            ctf_data.append({
-                'title': event_name,
-                'date': event_date,
-                'format': event_format,
-                'location': event_location,
-                'weight': event_weight,
-                'notes': event_notes,
-                'link': 'https://ctftime.org' + event_link # recreate the link
-            })
-
-        with open(filename, 'w') as json_file:
-            json.dump(ctf_data, json_file, indent=4)
-        print(f"CTF data has been saved to {filename}")
-
-        return ctf_data
-    
-    except Exception as e:
-        print(f"An error occurred: {e}")
-        return None
-
-def register_new_event(ctx: discord.Integration, CURRENT_CTF_DIR: str, data: json):
-    if (not p.isdir(f"{CURRENT_CTF_DIR}{ctx.guild.id}")):
-        mkdir(f"{CURRENT_CTF_DIR}{ctx.guild.id}")
-
-
-   
-    
 #look inside the Json data files for match
-async def search_ctf_data(filename, query, WEIGHT_RANGE):
+async def search_ctf_data(filename: str, query: str, WEIGHT_RANGE: int):
     
     match = [] # array of match
     with open(filename) as json_file:
@@ -212,8 +143,6 @@ async def search_event_data_by_role(filename: str, role: str):
 
     return None
 
-
-
 # creates & returns an embedded message based on the event_info given (in the format of ctf_events.json), the integer is to differenciate the color/format. 
 async def send_event_info(event_info, id: int):
 
@@ -272,14 +201,15 @@ async def send_event_info(event_info, id: int):
     embeded_message.add_field(name="End time", value=f"**{formatted_end_time}**", inline=True)
     embeded_message.add_field(name="Duration", value=f"**{duration_str}**", inline=True)
     embeded_message.add_field(name="Status", value=f"**{status_str}**", inline=False)
-    embeded_message.set_image(url="https://cdn.discordapp.com/attachments/1167256768087343256/1202189774836731934/CTFREI_Banniere_920_x_240_px_1.png?ex=67162479&is=6714d2f9&hm=c649d21b2152c0200b9466a29c09a04865387410258c1c228c8df58db111c539&")
+    if not event_info['logo']:
+        embeded_message.set_image(url="https://cdn.discordapp.com/attachments/1167256768087343256/1202189774836731934/CTFREI_Banniere_920_x_240_px_1.png?ex=67162479&is=6714d2f9&hm=c649d21b2152c0200b9466a29c09a04865387410258c1c228c8df58db111c539&")
+    else:
+        embeded_message.set_image(url=event_info['logo'])
 
     return embeded_message
 
-
-
 # make an api call on a url and retrieves all the data, then put it in a file.
-def api_call(url, file):
+def api_call(url: str, filename: str):
     try:
         # url = "https://ctftime.org/api/v1/events/?limit=100"
 
@@ -297,10 +227,10 @@ def api_call(url, file):
         data = response.json()
 
         #print(data)
-        with open(file, 'w') as fp:
+        with open(filename, 'w') as fp:
              json.dump(data, fp, indent=4)
 
-        print(f"{url} data has been saved to {file}")
+        print(f"{url} data has been saved to {filename}")
         
         return data
 
